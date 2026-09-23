e705acf9e603910540c8c81c148d04491c861cd8
radahpeter-hue <peterssentongo61@gmail.com>
2026-09-23 14:52:03 +0300

    fix: align organisation type constants and district spelling

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const UGANDA_DISTRICTS = [
   "Abim", "Adjumani", "Agago", "Alebtong", "Amolatar", "Amudat", "Amuria", "Amuru", "Apac", "Arua",
   "Budaka", "Bududa", "Bugiri", "Bugweri", "Buhweju", "Buikwe", "Bukedea", "Bukomansimbi", "Bukwo", "Bulambuli", "Buliisa", "Bundibugyo", "Bunyangabu", "Bushenyi", "Busia", "Butaleja", "Butambala", "Butebo", "Buvuma", "Buyende",
   "Dokolo", "Gomba", "Gulu", "Hoima", "Ibanda", "Iganga", "Isingiro", "Jinja", "Kaabong", "Kabale", "Kabarole", "Kaberamaido", "Kagadi", "Kakumiro", "Kalangala", "Kaliro", "Kalungu", "Kampala", "Kamuli", "Kamwenge", "Kanungu", "Kapchorwa", "Kapelebyong", "Kasanda", "Kasese", "Katakwi", "Kayunga", "Kibaale", "Kiboga", "Kibuku", "Kikuube", "Kiruhura", "Kiryandongo", "Kisoro", "Kitgum", "Koboko", "Kole", "Kotido", "Kumi", "Kwania", "Kween", "Kyankwanzi", "Kyegegwa", "Kyenjojo", "Kyotera",
-  "Lamwo", "Lira", "Luuka", "Lwengo", "Lyantonde", "Madi-Okollo", "Manafwa", "Maracha", "Masaka", "Masindi", "Mayuge", "Mbale", "Mbarara", "Mitooma", "Mityana", "Moroto", "Moyo", "Mpigi", "Mubende", "Mukono", "Nabilatuk", "Nakapiripirit", "Nakaseke", "Nakasongola", "Namayingo", "Namisindwa", "Namutumba", "Napak", "Nebbi", "Ngora", "Ntoroko", "Ntungamo", "Nwoya", "Obongi", "Omoro", "Otuke", "Oyam", "Pader", "Pakwach", "Pallisa", "Rakail", "Rubanda", "Rubirizi", "Rukiga", "Rukungiri", "Sembabule", "Serere", "Sheema", "Sironko", "Soroti", "Tororo", "Wakiso", "Yumbe", "Zombo"
+  "Lamwo", "Lira", "Luuka", "Lwengo", "Lyantonde", "Madi-Okollo", "Manafwa", "Maracha", "Masaka", "Masindi", "Mayuge", "Mbale", "Mbarara", "Mitooma", "Mityana", "Moroto", "Moyo", "Mpigi", "Mubende", "Mukono", "Nabilatuk", "Nakapiripirit", "Nakaseke", "Nakasongola", "Namayingo", "Namisindwa", "Namutumba", "Napak", "Nebbi", "Ngora", "Ntoroko", "Ntungamo", "Nwoya", "Obongi", "Omoro", "Otuke", "Oyam", "Pader", "Pakwach", "Pallisa", "Rakai", "Rubanda", "Rubirizi", "Rukiga", "Rukungiri", "Sembabule", "Serere", "Sheema", "Sironko", "Soroti", "Tororo", "Wakiso", "Yumbe", "Zombo"
 ].sort();
 
 export const PHARMA_CADRES = [
@@ -35,9 +35,9 @@ export const CONTACT_METHODS = [
 export const ORGANISATION_TYPES = [
   { id: 'retail_pharmacy', label: 'Retail Pharmacy' },
   { id: 'wholesale_pharmacy', label: 'Wholesale Pharmacy' },
-  { id: 'import_export', label: 'Import/Export' },
-  { id: 'manufacturing', label: 'Manufacturing' },
-  { id: 'hospital_pharmacy', label: 'Hospital Pharmacy' },
   { id: 'drug_shop', label: 'Licensed Drug Shop' },
+  { id: 'importer', label: 'Importer' },
+  { id: 'distributor', label: 'Distributor' },
+  { id: 'manufacturer', label: 'Manufacturer' },
   { id: 'other', label: 'Other' }
 ];
